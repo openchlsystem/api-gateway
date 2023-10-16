1057748c65069df8d8cbd5078e2ce31f8cf82401
kemboicheru <kemboicheru@gmail.com>
2023-10-16 17:00:43 +0300

    Update

--- holla/hollachoices.py
+++ holla/hollachoices.py
@@ -1,6 +1,5 @@
 import datetime
 import locale,random,string
-from django.contrib.sessions.models import Session
 from django.utils import timezone
 
 QUEUE_STATUS = {"0":"OFFLINE","1":"AVAILABLE","2":"ON BREAK","3":"PERSONAL BREAK","4":"COACHING AND TRAINING"}
@@ -139,21 +138,6 @@ def weekdates():
     
     dates=[str((start_date + datetime.timedelta(days=i)).date()) for i in range(7)]
     return dates
-
-def get_all_logged_in_users():
-    # Query all non-expired sessions
-    # use timezone.now() instead of datetime.now() in latest versions of Django
-    sessions = Session.objects.filter(expire_date__gte=timezone.now())
-    uid_list = []
-
-    # Build a list of user ids from that query
-    for session in sessions:
-        data = session.get_decoded()
-        uid_list.append(data.get('_auth_user_id', None))
-
-    # Query all logged in users based on id list
-    return uid_list
-    #return User.objects.filter(id__in=uid_list)
 
 def getRandomString(N=10):
     ran_str = ''.join(random.choices(string.ascii_uppercase +
